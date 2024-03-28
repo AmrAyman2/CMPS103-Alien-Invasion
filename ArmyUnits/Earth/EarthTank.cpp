acc84108d9f76e58afb4854858164f8cf1c36d7c
Abd El-Rahman Haydar <lboodyde02@gmail.com>
2024-03-28 23:08:21 +0200

    ISA

--- ArmyUnits/Earth/EarthTank.cpp
+++ ArmyUnits/Earth/EarthTank.cpp
@@ -1,1 +1,19 @@
 #include "EarthTank.h"
+void EarthTank::Attack()
+{
+	for (int i = 0; i < attackCapacity; i++)
+	{
+		monsterlist[i]->setHealth(health - power);
+		if (monsterList[i]->getHealth <= 0)
+			deathList->enqueue(monsterList[i]);
+		else if (monsterList[i]->getHealth > 0)
+			tempList->enqueue(monsterList[i]);
+	}
+	AlienMonster temp;
+	for (int i = 0; i < attackCapacity; i++)
+	{
+		tempList->dequeue(temp);
+		monsterList->insert(temp);
+	}
+
+}
--- ArmyUnits/Earth/EarthTank.h
+++ ArmyUnits/Earth/EarthTank.h
@@ -1,5 +1,7 @@
 #pragma once
 class EarthTank
 {
+public:
+	void Attack();
 };
 
