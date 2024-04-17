1931b66362fb1c527c6dd7e1ab3d9120b2b70ca7
Amr Ayman <amr.mohamed05@eng-st.cu.edu.eg>
2024-04-17 16:02:33 +0200

    ..

--- AlienArmy.h
+++ AlienArmy.h
@@ -7,26 +7,26 @@
 #include "ADS/LinkedQueue.h"
 class AlienArmy
 {
-	private:
-		LinkedQueue<AlienSoldier*> AS_List;
-		Deque<AlienDrone*> AD_List;
-		ArrayBag<AlienMonster*> AM_List;
-	public:
-		AlienArmy() {};
-		bool AddUnit(AlienSoldier* unit);
-		bool AddUnit(AlienDrone* unit);
-		bool AddUnit(AlienMonster* unit);
-		//void Attack(EarthArmy* earth);
-		int AS_Count();
-		void AS_PrintID();
-		int AD_Count();
-		void AD_PrintID();
-		int AM_Count();
-		void AM_PrintID();
-		int getTotalCount();
-		void print();
+		private:
+			LinkedQueue<AlienSoldier*> AS_List;
+			Deque<AlienDrone*> AD_List;
+			ArrayBag<AlienMonster*> AM_List;
+		public:
+			AlienArmy() {};
+			bool AddUnit(AlienSoldier* unit);
+			bool AddUnit(AlienDrone* unit);
+			bool AddUnit(AlienMonster* unit);
+			//void Attack(EarthArmy* earth);
+			int AS_Count();
+			void AS_PrintID();
+			int AD_Count();
+			void AD_PrintID();
+			int AM_Count();
+			void AM_PrintID();
+			int getTotalCount();
+			void print();
 
-		LinkedQueue<AlienSoldier*> getAS_List();
-		Deque<AlienDrone*> getAD_List();
-		ArrayBag<AlienMonster*> getAM_List();
+			LinkedQueue<AlienSoldier*> getAS_List();
+			Deque<AlienDrone*> getAD_List();
+			ArrayBag<AlienMonster*> getAM_List();
 };
