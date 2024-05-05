0a06b327e4305f9b480546fef72dbe638651f17d
Amr Ayman <amr.mohamed05@eng-st.cu.edu.eg>
2024-05-05 14:20:07 +0300

    ..

--- ArmyUnits/Alien/AlienSoldier.cpp
+++ ArmyUnits/Alien/AlienSoldier.cpp
@@ -12,10 +12,12 @@ void AlienSoldier::Attack(GameRules* game,EarthArmy* army)
 	while (army->getES_List().dequeue(unitattacked) && count < attackCapacity) {
 		int* unitattackedhp = unitattacked->getHealth();
 		*unitattackedhp -= (power * health / 100) / sqrt(*unitattackedhp);
-		if (*unitattacked->getHealth() <= 0) {
+		if (*unitattackedhp <= 0) {
 			game->getkilledlist().enqueue(unitattacked);
 			unitattacked->setTD(game->gettimeStep());
 		}
+		else if (*unitattackedhp > 0 && *unitattackedhp < (0.2 * unitattacked->getjoinHealth()));
+
 		else
 			army->getES_List().enqueue(unitattacked);
 		count++;
